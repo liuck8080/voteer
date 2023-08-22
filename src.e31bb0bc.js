d641490655d6e331746615db13912b08792b20c7
Liu Chuankai <liuck8080@163.com>
2023-08-22 20:04:51 +0800

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -48202,6 +48202,7 @@ function Election({
   }) => {
     const [data, setData] = (0, _react.useState)(options);
     function reload() {
+      checkedOptions = [];
       window.contract.get_candidates({
         election_id: id
       }).then(data => setData(data));
@@ -48457,7 +48458,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46167" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34519" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
